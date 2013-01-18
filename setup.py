651bcfbacc1b2a593a9e62c44f9879ab9b94ad83
Jonas Obrist <ojiidotch@gmail.com>
2013-01-18 18:08:20 +0900

    add python 3 support classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Topic :: Utilities",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
