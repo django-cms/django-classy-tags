ab1d10e2cd50e8d58400b3cf9c87e39627357a21
Mark Walker <theshow@gmail.com>
2022-01-22 18:52:27 +0000

    Release 3.0 (#68)
    * Turn on bumpversion commits
    
    * Bump version: 2.0.0 → 3.0.0
    
    * Fix #58

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-classy-tags',
-    version='2.0.0',
+    version='3.0.0',
     author='Jonas Obrist',
     author_email='ojiidotch@gmail.com',
     maintainer='Django CMS Association and contributors',
@@ -41,7 +41,7 @@ setup(
     license='BSD',
     description='Class based template tags for Django',
     long_description=open('README.rst').read(),
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     python_requires='>=3.7',
     include_package_data=True,
     zip_safe=False,
