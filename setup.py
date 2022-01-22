b4a9990358379f004f76bd1faa6997e4e984a476
Mark Walker <mark.walker@realbuzz.com>
2022-01-22 14:31:06 +0000

    Fix #58

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     license='BSD',
     description='Class based template tags for Django',
     long_description=open('README.rst').read(),
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     python_requires='>=3.7',
     include_package_data=True,
     zip_safe=False,
